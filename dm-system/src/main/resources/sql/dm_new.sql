4dac557fb22584cd14c5f860ec185ea58dc7226b
lizihang <lizihang@snsoft.com.cn>
2021-02-22 17:31:43 +0800

    修改

--- dm-system/src/main/resources/sql/dm_new.sql
+++ dm-system/src/main/resources/sql/dm_new.sql
@@ -6,10 +6,10 @@ create table dm_role
     role_code   varchar(64)   not null comment '角色编码',
     role_name   varchar(64)   not null comment '角色名称',
     status      int default 0 not null comment '状态：0-创建；1-正常；2-禁用；3-删除',
-    create_time datetime      null comment '创建时间',
     create_user varchar(64)   null comment '创建人',
-    modify_time datetime      null comment '修改时间',
-    modify_user varchar(64)   null comment '修改人'
+    create_time datetime      null comment '创建时间',
+    modify_user varchar(64)   null comment '修改人',
+    modify_time datetime      null comment '修改时间'
 )
 comment '角色表';
 
@@ -23,10 +23,10 @@ create table dm_user
     nickname    varchar(64)   not null comment '昵称',
     email       varchar(64)   not null comment '邮箱',
     status      int default 0 not null comment '状态：0-创建；1-正常；2-禁用；3-删除',
-    create_time datetime      null comment '创建时间',
     create_user varchar(64)   null comment '创建人',
-    modify_time datetime      null comment '修改时间',
-    modify_user varchar(64)   null comment '修改人'
+    create_time datetime      null comment '创建时间',
+    modify_user varchar(64)   null comment '修改人',
+    modify_time datetime      null comment '修改时间'
 )
 comment '用户表';
 
