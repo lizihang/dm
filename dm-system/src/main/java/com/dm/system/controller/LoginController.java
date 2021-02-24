8114199c7d6366b4a7ec3ee366752b14eb41ca9f
lizihang <lizihang@snsoft.com.cn>
2021-02-24 17:36:46 +0800

    修改代码结构

--- dm-system/src/main/java/com/dm/system/controller/LoginController.java
+++ dm-system/src/main/java/com/dm/system/controller/LoginController.java
@@ -1,8 +1,8 @@
-package com.dm.user.controller;
+package com.dm.system.controller;
 
 import com.dm.common.vo.Result;
 import com.dm.log.annotation.DmLog;
-import com.dm.user.util.ValidateCodeUtil;
+import com.dm.system.utils.ValidateCodeUtil;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,8 +18,8 @@ import java.util.Map;
  * <p>作者：lizh</p>
  * <p>审核：</p>
  * <p>重构：</p>
- * <p>创建日期：2020年10月19日 21:41</p>
- * <p>类全名：com.dm.user.controller.LoginController</p>
+ * <p>创建日期：2021年02月24日 17:01</p>
+ * <p>类全名：com.dm.system.controller.LoginController</p>
  * 查看帮助：<a href="" target="_blank"></a>
  */
 @CrossOrigin //解决跨域问题
