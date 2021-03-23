d1b950b15bcdec7541098a420f62ded6781d6d0a
lizihang <lzhsmart@163.com>
2021-03-23 19:50:38 +0800

    规范util工具类路径及名称

--- dm-common/src/main/java/com/dm/common/util/DateUtil.java
+++ dm-common/src/main/java/com/dm/common/util/DateUtil.java
@@ -1,8 +1,10 @@
-package com.dm.common.utils;
+package com.dm.common.util;
+
+import org.apache.commons.lang3.time.DateUtils;
 
 import java.util.Date;
 /**
- * <p>标题：时间工具类</p>
+ * <p>标题：</p>
  * <p>功能：</p>
  * <pre>
  * 其他说明：
@@ -10,11 +12,11 @@ import java.util.Date;
  * <p>作者：lizh</p>
  * <p>审核：</p>
  * <p>重构：</p>
- * <p>创建日期：2020年10月13日 16:30</p>
- * <p>类全名：com.dm.common.utils.DateUtils</p>
+ * <p>创建日期：2021年03月23日 19:23</p>
+ * <p>类全名：com.dm.common.util.DateUtil</p>
  * 查看帮助：<a href="" target="_blank"></a>
  */
-public class DateUtils extends org.apache.commons.lang3.time.DateUtils
+public class DateUtil extends DateUtils
 {
 	/**
 	 * 获取当前日期
