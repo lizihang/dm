efd0b1c48e19a57ab070c3acdb5a44ace05e9289
lizihang <lzhsmart@163.com>
2021-05-29 15:34:18 +0800

    修改

--- dm-system/src/main/java/com/dm/system/util/JwtTokenUtil.java
+++ dm-system/src/main/java/com/dm/system/util/JwtTokenUtil.java
@@ -25,7 +25,7 @@ import java.util.Map;
  * <p>作者：lizh</p>
  * <p>审核：</p>
  * <p>重构：</p>
- * <p>创建日期：2021年02月21日 15:20</p>
+ * <p>创建日期：2021年05月29日 13:47</p>
  * <p>类全名：com.dm.system.util.JwtTokenUtil</p>
  * 查看帮助：<a href="" target="_blank"></a>
  */
--- dm-system/src/main/java/com/dm/system/util/ServletUtil.java
+++ dm-system/src/main/java/com/dm/system/util/ServletUtil.java
@@ -17,7 +17,7 @@ import java.io.IOException;
  * <p>作者：lizh</p>
  * <p>审核：</p>
  * <p>重构：</p>
- * <p>创建日期：2020年12月09日 15:41</p>
+ * <p>创建日期：2021年05月29日 13:48</p>
  * <p>类全名：com.dm.system.util.ServletUtil</p>
  * 查看帮助：<a href="" target="_blank"></a>
  */
